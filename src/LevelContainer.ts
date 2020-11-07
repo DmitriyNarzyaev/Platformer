c1214589764da174444f49023e3446a5a587bb8d
DmitriyNarzyaev <nozaki@yandex.ru>
2020-11-07 18:12:28 +0300

    initial player

--- src/LevelContainer.ts
+++ src/LevelContainer.ts
@@ -1,20 +1,30 @@
 import Container = PIXI.Container;
 import { Texture, TilingSprite } from "pixi.js";
+import { Player } from "./Player";
 
 export default class LevelContainer extends Container {
 	public static readonly WIDTH:number = 4000;
 	public static readonly HEIGHT:number = 1500;
-	private _background:TilingSprite;
+    private _background:TilingSprite;
+    public static PLAYER_1:Player;
 
 	constructor() {
 		super();
 
         this.initBackground();
+        this.initPlayer();
 	}
 
 	private initBackground():void {
 		this._background = this.addChild(new TilingSprite(Texture.from("bg.png"), 100, 100));
 		this._background.width = LevelContainer.WIDTH;
 		this._background.height = LevelContainer.HEIGHT;
+    }
+    
+    private initPlayer():void {
+		LevelContainer.PLAYER_1 = new Player();							//PLAYER
+		this.addChild(LevelContainer.PLAYER_1);
+		LevelContainer.PLAYER_1.x = 100;
+		LevelContainer.PLAYER_1.y = 100;
 	}
 }
---
+++ src/Player.ts
@@ -0,0 +1,19 @@
+import { Container, Sprite } from "pixi.js";
+
+export class Player extends Container {
+
+	public static PLAYER_CONTAINER:PIXI.Container;
+	public static PLAYER_SPRITE:PIXI.Sprite;
+
+	constructor() {
+		super();
+
+		Player.PLAYER_CONTAINER = new PIXI.Container;
+		this.addChild(Player.PLAYER_CONTAINER);
+		Player.PLAYER_SPRITE = Sprite.from("player");
+		Player.PLAYER_CONTAINER.addChild(Player.PLAYER_SPRITE);
+		Player.PLAYER_SPRITE.width /= 3;
+		Player.PLAYER_SPRITE.height /= 3;
+
+    }
+}
